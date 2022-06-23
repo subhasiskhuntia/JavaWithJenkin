20c06d0ea8d866183a2d18fb63488c0d535dd21c
subhasiskhuntia <subhasiskhuntia506@gmail.com>
2022-06-23 18:31:47 +0530

    java program

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo{
     public static void main(String[] args) {
         System.out.println("running java applicaton");
         System.out.println("second messaage");
+        System.out.println("pushing the third message");
     }
 }
